7f5b0be146895de1797ad7bb653b4a88c7d9d47a
ArteXI <thomasaserfroc@yahoo.fr>
2021-05-12 04:40:20 +0200

    Design

--- Assets/WaterTrail.cs
+++ Assets/WaterTrail.cs
@@ -2,15 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WaterTrail : MonoBehaviour
 {
+    public Boat boat;
+    public float emissionFlowTimer;
+    public float emissionFlowTimer_current;
+    TrailRenderer[] trails;
     private void Start()
     {
-        TrailRenderer[] trails = GetComponentsInChildren<TrailRenderer>();
+        boat = GameObject.Find("Boat").GetComponent<Boat>();
+        trails = GetComponentsInChildren<TrailRenderer>();
         foreach(TrailRenderer t in trails)
         {
             t.startWidth = 0.3f;
             t.endWidth = 1;
+            emissionFlowTimer = 0;
+            emissionFlowTimer_current = 0;
+
         }
+    }
+
+    private void Update()
+    {
+        emissionFlowTimer_current += Time.deltaTime;
+        switch(emissionFlowTimer_current > emissionFlowTimer)
+        {
+            case true:
+                switch (boat.sailState)
+                {
+                    case SailsState.CLOSE:
+                        emissionFlowTimer = 0.3f;
+                        foreach (TrailRenderer t in trails)
+                        {
+                            t.emitting = (t.emitting == true) ? false : true;
+                        }
+                        break;
+
+                    case SailsState.MID_OPEN:
+                        emissionFlowTimer = Random.Range(0.05f, 0.5f);
+                        foreach (TrailRenderer t in trails)
+                        {
+                            t.emitting = (t.emitting == true)? false : true;
+                        }
+                        break;
+
+                    case SailsState.FULL_OPEN:
+                        emissionFlowTimer = 1f;
+                        foreach (TrailRenderer t in trails)
+                        {
+                            t.emitting = true;
+                        }
+                        break;
+                }
+                emissionFlowTimer_current = 0;
+                break;
+        }
+
     }
 }
