2f6ef28a4b8430b2aafbe89eb4e44d2feca6cb19
ArteXI <thomasaserfroc@yahoo.fr>
2021-05-17 08:47:17 +0200

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,16 +6,28 @@ public enum PlayerState { SAILINGstate = 0, ISLANDstate }
 public class Player : MonoBehaviour
 {
     public PlayerState m_state;
-
+    public Boat boat;
+    public Island islandTarget;
     // Start is called before the first frame update
     void Start()
     {
-            
+        boat = GameObject.Find("Boat").GetComponent<Boat>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (boat.navigationState)
+        {
+            case (NavigationState.DOCKED):
+            {
+                    this.islandTarget = boat.dockedAt.GetComponent<Island>();
+                    if (Input.GetKeyDown(KeyCode.U))
+                    {
+                        islandTarget.levelUp();
+                    }
+                    break;
+            }
+        }
     }
 }
