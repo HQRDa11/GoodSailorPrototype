928bba375da73aa86006b53392a41c6ebd04dc08
ArteXI <thomasaserfroc@yahoo.fr>
2021-05-20 13:49:51 +0200

    Update Tools.cs

--- Assets/Scripts/Main/Tools.cs
+++ Assets/Scripts/Main/Tools.cs
@@ -9,4 +9,13 @@ public static class Tools
         Vector3 randomScale = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
         return randomScale;
     }
+
+    public static T[] FillArray<T>(T[] arr,T value)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = value;
+        }
+        return arr;
+    }
 }
